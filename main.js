91629f9c63cd0a6b29d4a0ce18774a02dc9820ec
Maria Ivanova <mariya.kl.ivanova@gmail.com>
2025-07-02 10:47:03 +0200

    JS Vanilla

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-async function GetAPIData() {
-    try {
-        const response = await fetch("https://dog.ceo/api/breeds/image/random");
-        if (response.ok) {
-            const data = await response.json();
-            document.getElementById("dogimg").src = data["message"];
-        } else {
-            console.error(`HTTP error ${response.status}`);
-        }
-    } catch (error) {
-        console.error("Fetch failed:", error);
-    }
-}
-
-GetAPIData();
+document.getElementById("dowork").addEventListener("click", function (e) {
+    document.getElementById("welcome").innerHTML = "Welcome " + document.getElementById("name").value;
+});
