bc3812c3c0ef1ebef99e41476b9ad2b351aa5d42
Maria Ivanova <mariya.kl.ivanova@gmail.com>
2025-07-02 10:35:42 +0200

    Add API functionality to fetch and display data

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Maria Ivanova"); 
+async function GetAPIData() {
+    try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        if (response.ok) {
+            const data = await response.json();
+            document.getElementById("dogimg").src = data["message"];
+        } else {
+            console.error(`HTTP error ${response.status}`);
+        }
+    } catch (error) {
+        console.error("Fetch failed:", error);
+    }
+}
+
+GetAPIData();
