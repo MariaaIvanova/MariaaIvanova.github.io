5e6a0b253d31b416ef6fe4f48a4e75cd5813ff1f
Maria Ivanova <mariya.kl.ivanova@gmail.com>
2025-07-02 11:02:48 +0200

    Add Bored API integration

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-document.getElementById("dowork").addEventListener("click", function (e) {
-    document.getElementById("welcome").innerHTML = "Welcome " + document.getElementById("name").value;
-});
+async function GetAPIData() {
+    try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        if (response.ok) {
+            const data = await response.json();
+            //document.getElementById("dogimg").src = data["message"];
+            let imgel = document.createElement("img");
+            imgel.src =  data["message"];
+            document.getElementsByTagName("body")[0].appendChild(imgel);
+        } else {
+            console.error(`HTTP error ${response.status}`);
+        }
+    } catch (error) {
+        console.error("Fetch failed:", error);
+    }
+}
+
+for (let i = 0; i < 10; i++)
+    GetAPIData();
